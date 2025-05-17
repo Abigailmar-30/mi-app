815bedd7ba34af2088cf1c086c6608a2fbdd5655
mi proyecto <abigailmtzm2002@gmail.com>
2025-05-17 14:30:29 -0600

    mi proyecto 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//----------- declaracion de rutas fijas ----------------//
+Route::get('/Productos',function(){
+    return "Seccion de productos";
+});
+//----------- declaracion con parametros ----------------//
+
+Route::get('Productos/{id}', function ($id){
+    return "Bienvenido $id";
+});
+//----------- declaracion de rutas fijas ----------------//
+
+Route::get('prueba parametros/{id}', function ($id){
+    $d=$id;
+    $nombre = explode("-",$d);
+    return "tu nombre es $nombre[1]";
+});
